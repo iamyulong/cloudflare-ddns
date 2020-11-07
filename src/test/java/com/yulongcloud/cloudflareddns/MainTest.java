9b56f7ce393893fb02983851cab30826fba23cdb
Yulong Wu <yulongwu@outlook.com>
2020-11-07 22:01:27 +0000

    Set up gradle build

---
+++ src/test/java/com/yulongcloud/cloudflareddns/MainTest.java
@@ -0,0 +1,12 @@
+package com.yulongcloud.cloudflareddns;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+    @Test
+    public void testNothing() {
+        assertTrue(true);
+    }
+}
